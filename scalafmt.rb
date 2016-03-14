1a547e557a6b6282bc80d3267dc6ffa2b77b9dc4
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-03-14 13:50:44 +0100

    Fix typo.

--- scalafmt.rb
+++ scalafmt.rb
@@ -1,5 +1,5 @@
 class Scalafmt < Formula
-  desc "Optinionated code formatter for Scala."
+  desc "Opinionated code formatter for Scala."
   homepage "https://github.com/olafurpg/scalafmt"
   url "https://github.com/olafurpg/scalafmt/releases/download/v0.1.3/scalafmt.tar.gz"
   sha256 "3e11cd4fa0001a974cc6ded5bdfb8303d4ad507b22a97d971820beb894f38450"
