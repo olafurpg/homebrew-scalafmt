046d6c7df2208fdfacacf7356be1aa61cdb5f25d
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-08-13 22:42:52 +0200

    Update to v0.3.0-RC1

--- scalafmt.rb
+++ scalafmt.rb
@@ -1,9 +1,9 @@
 class Scalafmt < Formula
   desc "Code formatter for Scala."
   homepage "https://github.com/olafurpg/scalafmt"
-  url "https://github.com/olafurpg/scalafmt/releases/download/v0.2.12/scalafmt.tar.gz"
-  sha256 "21669b4617192a397baa8c940875ab644c98dfb1833aee30c69b78aa18de06cc"
-  version "0.2.12"
+  url "https://github.com/olafurpg/scalafmt/releases/download/v0.3.0-RC1/scalafmt.tar.gz"
+  sha256 "3ac819b5f020fb67a15ede491775558ec8fd82774a0d8bde28ff5a0cff1ce1dc"
+  version "0.3.0-RC1"
 
   def install
     prefix.install "cli/target/scala-2.11/scalafmt.jar"
