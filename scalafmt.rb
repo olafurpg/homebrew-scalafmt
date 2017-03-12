91d3d288f72f3259ac3a1f2f075d8e36d8378233
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-03-12 21:22:17 +0100

    Update homepage

--- scalafmt.rb
+++ scalafmt.rb
@@ -1,7 +1,7 @@
 class Scalafmt < Formula
   desc "Code formatter for Scala."
-  homepage "https://github.com/olafurpg/scalafmt"
-  url "https://github.com/olafurpg/scalafmt/releases/download/v0.6.2/scalafmt.tar.gz"
+  homepage "https://github.com/scalameta/scalafmt"
+  url "https://github.com/scalameta/scalafmt/releases/download/v0.6.2/scalafmt.tar.gz"
   sha256 "9810f4fe5e29d71c51eaa2c9fe282562413b8ec31dea14af62be315bf7a14e75"
   version "0.6.2"
 
