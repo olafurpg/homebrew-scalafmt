65118f584e3364874a2c7b44f2e6c2f2555ed813
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-03-14 18:03:56 +0100

    Use old location for one more release

--- scalafmt.rb
+++ scalafmt.rb
@@ -1,7 +1,7 @@
 class Scalafmt < Formula
   desc "Code formatter for Scala."
   homepage "https://github.com/scalameta/scalafmt"
-  url "https://github.com/scalameta/scalafmt/releases/download/v0.6.3/scalafmt.tar.gz"
+  url "https://github.com/olafurpg/scalafmt/releases/download/v0.6.3/scalafmt.tar.gz"
   sha256 "eb5fff3f7daafb5be5a85435ad70c3c7c0fd3f392e46b2d99da135e33b6cb4c5"
   version "0.6.3"
 
