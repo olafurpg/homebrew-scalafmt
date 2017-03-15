881c28cf1d8b7e5a07a6aa8eab6d20a88d0e4ca8
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-03-15 09:03:42 +0100

    Fix url

--- scalafmt.rb
+++ scalafmt.rb
@@ -1,7 +1,7 @@
 class Scalafmt < Formula
   desc "Code formatter for Scala."
   homepage "https://github.com/scalameta/scalafmt"
-  url "https://github.com/olafurpg/scalafmt/releases/download/v0.6.4/scalafmt.tar.gz"
+  url "https://github.com/scalameta/scalafmt/releases/download/v0.6.4/scalafmt.tar.gz"
   sha256 "4112e971c35761ded03d956be0549a799f5452a636356665484d43a5f694136c"
   version "0.6.4"
 
