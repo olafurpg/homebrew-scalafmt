b6767a3f2a1b280f67ba8525d5a39154fc791ca1
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-04-25 17:56:09 +0200

    Add simple install script for scalafmt

--- scalafmt.rb
+++ scalafmt.rb
@@ -1,18 +1,8 @@
-class Scalafmt < Formula
-  desc "Code formatter for Scala."
-  homepage "https://github.com/scalameta/scalafmt"
-  url "https://github.com/scalameta/scalafmt/releases/download/v0.7.0-RC1/scalafmt.tar.gz"
-  sha256 "0c1ff94acfe3fc996a6bf6fbadc022a8dd4034300f47ad369fd8eabd9c30cad9"
-  version "0.7.0-RC1"
+require 'formula'
 
+class Coursier < Formula
+  head 'git://github.com/scalameta/scalafmt.git'
   def install
-    prefix.install "scalafmt-cli/target/scala-2.12/scalafmt.jar"
-    system "./bin/configure"
-    inreplace "bin/scalafmt", "PATH_TO", prefix
-    bin.install "bin/scalafmt"
-  end
-
-  test do
-    system "false"
+    bin.install 'scalafmt'
   end
 end
